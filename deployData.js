45e7b69abba30c44aa7aabad8e6d53439059c64a
luthfi alwan azhari <luthfialwan2@gmail.com>
2023-05-21 16:30:45 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Luthfi Alwan Azhari"; // TODO: replace this
+
+const studentId = "FE4881660"; // TODO: replace this
+
+const NetlifyDeployUrl = "TODO: replace me"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
